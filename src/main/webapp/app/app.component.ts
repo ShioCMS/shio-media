e30440d1ad32c371f65531563a523c14f0a0a1dd
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-04-03 11:57:34 -0300

    - Using scss instead of css

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -6,7 +6,7 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit  {
   title = 'shiohara-stock';
--- src/main/webapp/app/components/top-header/top-header.component.ts
+++ src/main/webapp/app/components/top-header/top-header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-top-header',
   templateUrl: './top-header.component.html',
-  styleUrls: ['./top-header.component.css']
+  styleUrls: ['./top-header.component.scss']
 })
 export class TopHeaderComponent implements OnInit {
 
